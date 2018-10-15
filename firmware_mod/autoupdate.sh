9380037d478e0dda9874ed1b8e788f26a5f4832f
Scott Van Hess <scottvanhess@invisionapp.com>
2018-10-15 12:13:53 -0700

    Set repo

--- firmware_mod/autoupdate.sh
+++ firmware_mod/autoupdate.sh
@@ -6,7 +6,7 @@
 # Depends on curl, jq (json parser), openssl (SHA calculation)
 
 # owner name and repo name
-REPO="EliasKotlyar/Xiaomi-Dafang-Hacks"
+REPO="svanhess/Xiaomi-Dafang-Hacks"
 # Branch to use
 BRANCH="master"
 # Initial remote folder
